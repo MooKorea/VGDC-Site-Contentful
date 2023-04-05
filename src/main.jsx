2869d6879b29d72915e0c803289a60562887bde3
MooKorea <69235304+MooKorea@users.noreply.github.com>
2023-04-04 22:11:33 -0500

    remove GTM module

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from "react-router-dom"
 
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-NKWBW9G'
+}
+
+TagManager.initialize(tagManagerArgs)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
