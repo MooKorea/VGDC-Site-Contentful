efc1363bf9005cc81e9cf815183fdd13af13a6eb
MooKorea <69235304+MooKorea@users.noreply.github.com>
2023-04-06 20:22:28 -0500

    homepage desktop fix

--- src/pages/Home/Paragraph.jsx
+++ src/pages/Home/Paragraph.jsx
@@ -1,31 +1,11 @@
 import React, { useRef, useState, useEffect } from "react";
-import { useMediaQuery } from "../../hooks/useMediaQuery";
 
 export default function Paragraph() {
-  const textHeight = useRef();
-  const mediaQuery = useMediaQuery("lg");
-  const handleMediaQuery = (big, small, size) => {
-    const getHeight = textHeight.current?.offsetHeight.toString();
-    let sizeSelect = mediaQuery;
-    if (size === "medium") sizeSelect = mediaQueryMd;
-    if (sizeSelect) {
-      return {
-        height: `${getHeight}px`,
-        clipPath: "polygon(0 6%, 100% 0, 100% 100%, 0 94%)",
-      };
-    } else {
-      return {
-        height: `${getHeight}px`,
-        clipPath: "polygon(0 6%, 100% 0, 100% 100%, 0 94%)",
-      };
-    }
-  };
-
   return (
-    <div className="home-paragraph-container" style={handleMediaQuery()}>
-      <div className="home-paragraph" ref={textHeight}>
+    <div className="home-paragraph-container">
+      <div className="home-paragraph">
         <div>
-          <h2>We make stuff</h2>
+          <h2>Learn with us!</h2>
           <p>
             Welcome to the Video Game Development Club! We are actors, artists, designers,
             musicians, programmers, writers, and more! All skill levels are welcome, and
